8a28833a6813bfd7b419c60ccd79a22e6c1c37c8
emil.archacki <emil.archacki@bms.com.pl>
2022-02-19 22:58:55 +0100

    pierwszy commit

---
+++ homeworks/homework5.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import time
+
+url = 'http://api.currencylayer.com/live'
+parameters = {'access_key':'23334123d578c86369a91e2ed47c1d38', 'source':'USD', 'currencies':'PLN'}
+x = 0
+
+def GetResponse(url, parameters):
+    try:
+        # strzelamy requestem i pobieramy responsa
+        response = requests.get(url, params = parameters)
+        # zamieniamy responsa (który jest stringiem) w słownik
+        data = response.json()
+        return response, data
+
+    except NameError:
+        data = 'Błąd pozyskania danych'
+        return response, data
+
+def DateTimeMeasure(response, data):
+    try:
+        # pobranie czasu wykonania zapytania, przeliczenie na milisekundy oraz zaokrąglenie
+        requestTime = str((round((response.elapsed.total_seconds() * 1000))))
+        # godzina wykonania zapytania
+        requestDateAndHour = str(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+        # NIEPOTRZEBNE! pobranie timestapm kursu z responsa i sformatowanie go do daty
+        # requestDate = str((datetime.fromtimestamp(data['timestamp'])))
+        return requestTime, requestDateAndHour
+
+    except NameError:
+        print('Brak danych z responsa')
+
+def PrintAndWriteRates(url, parameters):
+    # pobieramy responsa oraz dane z niego
+    response, data = GetResponse(url, parameters)
+    # pobieramy czas zapytania i datę zapytania
+    requestTime, requestDateAndHour = DateTimeMeasure(response, data)
+    # pobieramy kurs
+    quotes = str((data['quotes']['USDPLN']))
+
+    print('========================================================')
+    print('Kurs dolara: ' + quotes)
+    print('Data i godzina: ' + requestTime)
+    print('Czas wykonania zapytania: ' + requestDateAndHour)
+
+    # otwieramy, gdy brak to tworzymy plik, a następnie dopisujemy ostatnią linię
+    file = open('C:/Temp/currency.csv', 'a+')
+    file.write(str(quotes) + ',' + str(requestTime) + ',' + str(requestDateAndHour) + '\n')
+    file.close()
+
+while x < 2:
+    PrintAndWriteRates(url, parameters)
+    x += 1
+    time.sleep(3)
+
+
+#print(response.url)
+#print(response.text)
+# print(quotes)
+# print(requestTime)
+# print(requestDate)
